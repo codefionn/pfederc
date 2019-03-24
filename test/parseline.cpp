8d35848611a68ed1c92a41cb38c9ea2e849dbe14
Fionn Langhans <fionn.langhans@gmail.com>
2019-03-24 19:24:49 +0100

    Tests parse* expect tok_eof after expression.

--- test/parseline.cpp
+++ test/parseline.cpp
@@ -13,6 +13,11 @@ int main(int argsc, char * argsv[]) {
   auto line = parser::parse(lex);
   if (!line) return 1;
 
+  if (lex.currentToken() != lexer::tok_eof) {
+    std::cerr << "After parsing one expression: EOF not reached!" << std::endl;
+    return 1;
+  }
+
   std::cout << line->to_string() << std::endl;
   return 0;
 }
